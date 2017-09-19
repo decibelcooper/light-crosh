ef5f4528164bb24a20eddec7bc538059c16b0192
David Blyth <dblyth@anl.gov>
2017-09-18 19:43:00 -0500

    Create prefs.js

---
+++ prefs.js
@@ -0,0 +1,6 @@
+term_.prefs_.resetAll();
+
+term_.prefs_.set("background-color", "#ffffff");
+term_.prefs_.set("foreground-color", "#373b41");
+term_.prefs_.set("cursor-color",     "#373b41");
+term_.prefs_.set("color-palette-overrides", ["#1d1f21","#cc6666","#b5bd68","#f0c674","#81a2be","#b294bb","#8abeb7","#c5c8c6","#969896","#cc6666","#b5bd68","#f0c674","#81a2be","#b294bb","#8abeb7","#ffffff"]);
